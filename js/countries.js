848a9e33005e27297113db9cc487afc7d541a00c
Saurabh Yadav <91422555+arbok7@users.noreply.github.com>
2023-08-30 13:53:06 +0530

    Add files via upload

---
+++ js/countries.js
@@ -0,0 +1,19 @@
+const countries = {
+    "en-GB": "English",
+    "hi-IN": "Hindi",
+    "de-DE": "German",
+    "fr-FR": "French",
+    "ja-JP": "Japanese",
+    "ru-RU": "Russian",
+    "ar-SA": "Arabic",
+    "ne-NP": "Nepali",
+    "fi-FI": "Finnish",
+    "es-ES": "Spanish",
+    "gu-IN": "Gujarati",
+    "id-ID": "Indonesian",
+    "bn-IN": "Bengali",
+    "bo-CN": "Tibetan",
+    "el-GR": "Greek",
+    "ta-LK": "Tamil",
+    "th-TH": "Thai"
+}
